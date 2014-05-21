cd972845e72fe533328f2b49eaf96f6f4e297593
Mathias Rechtzigel <MathiasaurusRex@gmail.com>
2014-05-21 14:06:46 -0500

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -30,7 +30,9 @@ function walk(node)
 function handleText(textNode) 
 {
 	var v = textNode.nodeValue;
-
+	v = v.replace(/\bWunderman\b/g, "Ricky Martin");
+	v = v.replace(/\bwunderman\b/g, "Ricky Martin");
+	v = v.replace(/\bThe cloud\b/g, "My butt");
 	v = v.replace(/\bThe Cloud\b/g, "My Butt");
 	v = v.replace(/\bThe cloud\b/g, "My butt");
 	v = v.replace(/\bthe Cloud\b/g, "my Butt");
